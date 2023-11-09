0c77a3afdcdf3ba68beb57dbc6714c7253ba6acf
SiennaB11 <bila2440@mylaurier.ca>
2023-11-08 20:51:54 -0500

    elements

--- customelements.js
+++ customelements.js
@@ -5,3 +5,22 @@ this.innerHTML = "Sienna was here";
 }
 
 customElements.define("x-sienna", SiennaElement);
+
+class CenterElement extends HTMLElement{
+connectedCallback(){
+this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>
+`;
+
+}
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+connectedCallback(){
+this.innerHTML = new Date().getFullYear();
+
+}
+}
+
+customElements.define("x-fullyear", YearElement);
